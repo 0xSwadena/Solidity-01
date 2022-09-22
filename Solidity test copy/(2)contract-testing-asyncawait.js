961443f2b0136e574bc4f20ebccc9e7ef35dd1e1
0xSwadena <110315989+0xSwadena@users.noreply.github.com>
2022-09-22 13:14:40 +0800

    Update (2)contract-testing-asyncawait.js

--- Solidity test copy/(2)contract-testing-asyncawait.js
+++ Solidity test copy/(2)contract-testing-asyncawait.js
@@ -6,8 +6,11 @@ const solc = require('solc');
 
 let accounts;
 
+//the function that will pass to beforeEach is going to have asynchronous code inside of it
 beforeEach (async () => {
 // get a list of all account
+   //re-assign a value to the accounts var
+   //get a list of the account from 'web3' that will access 'eth' module
    accounts = await web3.eth.getAccounts()
 
 // Use one of those accounts to deploy the contract
